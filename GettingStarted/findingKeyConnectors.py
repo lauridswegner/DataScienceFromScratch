d06a23a735c71508d71a2292863f821df56bc3b6
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-06-28 21:53:31 +0200

    added friends_of_friends-function

--- GettingStarted/findingKeyConnectors.py
+++ GettingStarted/findingKeyConnectors.py
@@ -28,5 +28,34 @@ def number_of_friends(user):
     friend_ids = friendships[user_id]
     return len(friend_ids)
 
+# getting total of connections
 total_connections = sum(number_of_friends(user) for user in users)
-print(total_connections)
+print("total connections:", total_connections)
+
+# average connections
+num_users = len(users)
+avg_connections = total_connections/num_users
+print("avg connections:", avg_connections)
+
+# sorting from most to least friends
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+num_friends_by_id.sort(
+    key=lambda id_and_friends: id_and_friends[1],
+    reverse = True
+)
+print(num_friends_by_id)
+
+# finding friends of friends
+from collections import Counter
+
+def friends_of_friends(user):
+    user_id = user["id"]
+    return Counter(
+        foaf_id
+        for friend_id in friendships[user_id]           # for each of my friends
+        for foaf_id in friendships[friend_id]           # find their friends
+        if foaf_id != user_id                           # who aren't me
+        and foaf_id not in friendship_pairs[user_id]    # and aren't my friends
+    )
+
+print(friends_of_friends(users[3]))
