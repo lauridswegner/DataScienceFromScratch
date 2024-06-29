cdb910627b6b5adbb255f449532767688690cc43
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-06-29 16:36:21 +0200

    added mean, median and quantile in centralTendencies.py

---
+++ 5-Statistics/centralTendencies.py
@@ -0,0 +1,49 @@
+from typing import List
+from collections import Counter
+
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+def mean(xs: List[float]) -> float:
+    return sum(xs) / len(xs)
+
+mean(num_friends)
+
+# underscore indicates that these are "private" functions, as they are
+# intended to be called by our median function but not by other people
+# using our statistics library.
+def _median_odd(xs: List[float]) -> float:
+    """If len(xs) is odd, the median is the middle element"""
+    return sorted(xs)[len(xs) // 2]
+
+def _median_even(xs: List[float]) -> float:
+    """If len(xs) is even, it's the average of the middle two elements"""
+    sorted_xs = sorted(xs)
+    hi_midpoint = len(xs) // 2
+    return (sorted_xs[hi_midpoint - 1] + sorted_xs[hi_midpoint]) / 2
+
+def median(v: List[float]) -> float:
+    """Finds the middle-most value of v"""
+    return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
+
+assert median([1, 10, 2, 9, 5]) == 5
+assert median([1, 9, 2, 10]) == (2 + 9) / 2
+
+print(median(num_friends))
+
+def quantile(xs: List[float], p: float) -> float:
+    """Returns the pth-percentile value in xs"""
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
+def mode(xs: List[float]) -> List[float]:
+    """Returns a list, since there might be more than one mode"""
+    counts = Counter(xs)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items() if count == max_count]
+
+assert set(mode(num_friends)) == {1,6}
