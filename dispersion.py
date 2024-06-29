8d093b64b03f2871d5ee35aa47fb8232544414da
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-06-29 18:37:13 +0200

    added dispersion

--- dispersion.py
+++ dispersion.py
@@ -1,4 +1,7 @@
 from typing import List
+import math
+from vectors import sum_of_squares
+from centralTendencies import mean, quantile
 
 num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
@@ -6,9 +9,6 @@ def data_range(xs: List[float]) -> float:
     return max(xs) - min(xs)
 
 assert data_range(num_friends) == 99
-
-from vectors import sum_of_squares
-from centralTendencies import mean
 
 def de_mean(xs: List[float]) -> List[float]:
     """Translate xs by subtracting its mean (so the result has mean 0)"""
@@ -24,3 +24,15 @@ def variance(xs: List[float]) -> float:
     return sum_of_squares(deviations) / (n - 1)
 
 assert 81.54 < variance(num_friends) < 81.55
+
+def standard_deviation(xs: List[float]) -> float:
+    """The standard deviation is the square root of the variance"""
+    return math.sqrt(variance(xs))
+
+assert 9.02 < standard_deviation(num_friends) < 9.04
+
+def interquartile_range(xs: List[float]) -> float:
+    """Returns the difference between the 75%-ile  and the 25%-ile"""
+    return quantile(xs, 0.75) - quantile(xs, 0.25)
+
+assert interquartile_range(num_friends) == 6
