4dd6a8afc3c3ff9d312a35a4fa5b1822cb1129a8
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-07-05 15:02:54 +0200

    added continuousDistributions.py

---
+++ continousDistribution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def uniform_pdf(x: float) -> float:
+    return 1 if 0 <= x <= 1 else 0
+
+def uniform_cdf(x: float) -> float:
+    if x < 0:   return 0
+    elif x < 1: return x
+    else:       return 1
+
